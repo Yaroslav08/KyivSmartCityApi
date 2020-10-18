b21a1f7591d98d1e03e2ef19bac72c68d4ff1ff5
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-10-18 22:32:35 +0300

    Add BankCard entity

---
+++ KyivSmartCityApi/KyivSmartCityApi/Models/BankCard.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace KyivSmartCityApi.Models
+{
+    public class BankCard
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("mask")]
+        public string Mask { get; set; }
+        [JsonProperty("main")]
+        public bool Main { get; set; }
+        [JsonProperty("expired")]
+        public bool Expired { get; set; }
+    }
+}
---
+++ KyivSmartCityApi/KyivSmartCityApi/Models/ListBankCards.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace KyivSmartCityApi.Models
+{
+    public class ListBankCards
+    {
+        [JsonProperty("cards")]
+        public List<BankCard> BankCards { get; set; }
+    }
+}
