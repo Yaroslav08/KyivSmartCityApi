af9b7131acc70f96aa006aacbf37e67993f28b7b
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-10-18 15:31:42 +0300

    Add exmaple project

---
+++ KyivSmartCityApi/KyivSmartCityApi.Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KyivSmartCityApi.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
