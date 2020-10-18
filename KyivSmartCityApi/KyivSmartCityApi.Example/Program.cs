4cfe21756395469131a203a49676a5861bc7d120
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-10-18 19:46:28 +0300

    Add Document entity

--- KyivSmartCityApi/KyivSmartCityApi.Example/Program.cs
+++ KyivSmartCityApi/KyivSmartCityApi.Example/Program.cs
@@ -12,10 +12,7 @@ namespace KyivSmartCityApi.Example
         {
             Console.OutputEncoding = Encoding.UTF8;
             KyivApiClient client = new KyivApiClient("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvYXBwLmt5aXZjaXR5Lmdvdi51YVwvYXV0aFwvY2FsbGJhY2siLCJpYXQiOjE2MDMwMjk5NzYsImV4cCI6MTYwNTcwODM3NiwibmJmIjoxNjAzMDI5OTc2LCJqdGkiOiJJVFRhcWd3ZTZ4OVdIZHgzIiwic3ViIjoxMTA3MjEsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.Udsa9HNZuk6dzTfVa69zZulahgTJEjamFPcTpy7Xnxw");
-            //var feeds = await client.GetFeedsAsync();
-            //var feedItem = await client.GetFeedAsync(feeds.First().Id);
-            var cards = await client.GetTravelCardsAsync();
-            var trips = await client.GetTravelCardHistoryAsync(cards.SmartCards.Last().Id);
+            
         }
     }
 }
---
+++ KyivSmartCityApi/KyivSmartCityApi/Models/Document.cs
@@ -0,0 +1,29 @@
+﻿using KyivSmartCityApi.Settings;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace KyivSmartCityApi.Models
+{
+    public class Document
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("type")]
+        public string Type { get; set; }
+        [JsonProperty("type_text")]
+        public string TypeText { get; set; }
+        [JsonProperty("series")]
+        public string Series { get; set; }
+        [JsonProperty("number")]
+        public string Number { get; set; }
+        [JsonProperty("issueDate")]
+        [JsonConverter(typeof(DateTimeConverter))]
+        public DateTime IssueDate { get; set; }
+        [JsonProperty("issuedBy")]
+        public string IssuedBy { get; set; }
+        [JsonProperty("expiryDate")]
+        [JsonConverter(typeof(DateTimeConverter))]
+        public DateTime ExpiryDate { get; set; }
+    }
+}
