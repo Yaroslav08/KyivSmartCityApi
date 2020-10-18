2da9ce38b42833b8aa5da3a075820bd2bfec6975
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-10-18 15:56:00 +0300

    Added some entities

---
+++ KyivSmartCityApi/KyivSmartCityApi/Models/Body.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace KyivSmartCityApi.Models
+{
+    public class Body
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+}
---
+++ KyivSmartCityApi/KyivSmartCityApi/Models/FeedItem.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace KyivSmartCityApi.Models
+{
+    public class FeedItem
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("title")]
+        public string Title { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
+        [JsonProperty("icon")]
+        public string Icon { get; set; }
+        [JsonProperty("body")]
+        public List<Body> Body { get; set; }
+        public bool with_voting { get; set; }
+        public object voted_for { get; set; }
+        [JsonProperty("links")]
+        public List<Links> Links { get; set; }
+        [JsonProperty("feed_item")]
+        public Feed Feed { get; set; }
+    }
+}
