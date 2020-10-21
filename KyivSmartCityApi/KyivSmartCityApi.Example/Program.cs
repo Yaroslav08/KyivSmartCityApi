44a9fecfe0d356ba7c42724b6fb060bc02f83e02
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-10-21 23:04:27 +0300

    Removed access token

--- KyivSmartCityApi/KyivSmartCityApi.Example/Program.cs
+++ KyivSmartCityApi/KyivSmartCityApi.Example/Program.cs
@@ -12,10 +12,10 @@ namespace KyivSmartCityApi.Example
         static async Task Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            KyivApiClient client = new KyivApiClient("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvYXBwLmt5aXZjaXR5Lmdvdi51YVwvYXBpXC9hdXRoXC9yZWZyZXNoIiwiaWF0IjoxNjAzMTE4NTU4LCJleHAiOjE2MDU4NjgxMTksIm5iZiI6MTYwMzE4OTcxOSwianRpIjoidU05dWw4dzl0aWNQYm0yZCIsInN1YiI6MTEwNzIxLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.ub7wOdLAD8PG4r2a4OLb4dS_PuXj8MfhSL0sJppPYc8");
-            //var feeds = await client.GetFeedsAsync();
-            //var cards = await client.GetTravelCardsAsync();
-            //var card = await client.GetTravelCardHistoryAsync(cards.SmartCards.Last().Id);
+            KyivApiClient client = new KyivApiClient("");
+            var feeds = await client.GetFeedsAsync();
+            var cards = await client.GetTravelCardsAsync();
+            var card = await client.GetTravelCardHistoryAsync(cards.SmartCards.Last().Id);
         }
     }
 }
