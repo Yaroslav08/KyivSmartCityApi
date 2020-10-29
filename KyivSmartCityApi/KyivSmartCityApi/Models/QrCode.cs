488968d2a3a346758ddc09a829f0eb41cfe83cb5
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-10-29 20:36:20 +0200

    Added QrCode models

---
+++ KyivSmartCityApi/KyivSmartCityApi/Models/QrCode.cs
@@ -0,0 +1,22 @@
+﻿using KyivSmartCityApi.Settings;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace KyivSmartCityApi.Models
+{
+    public class QrCode
+    {
+        [JsonProperty("id")]
+        private long Id { get; set; }
+        [JsonProperty("code")]
+        private string Code { get; set; }
+        [JsonProperty("expireAt")]
+        [JsonConverter(typeof(UtcDateTimeConverter))]
+        private DateTime ExpireAt { get; set; }
+        [JsonProperty("shared")]
+        private bool IsShared { get; set; }
+        [JsonProperty("used")]
+        private bool IsUsed { get; set; }
+    }
+}
---
+++ KyivSmartCityApi/KyivSmartCityApi/Models/QrCodes.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace KyivSmartCityApi.Models
+{
+    public class QrCodes
+    {
+        [JsonProperty("total")]
+        public int Total { get; set; }
+        [JsonProperty("codes")]
+        public List<QrCode> Codes { get; set; }
+    }
+}
