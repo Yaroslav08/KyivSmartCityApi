a930624be4faf6eb8ec800cd56e9a44fbc03085e
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-10-29 20:54:45 +0200

    Added WalletReplenish entity

---
+++ KyivSmartCityApi/KyivSmartCityApi/Models/WalletReplenish.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace KyivSmartCityApi.Models
+{
+    public class WalletReplenish
+    {
+        [JsonProperty("balance_wallet")]
+        public int BalanceWallet { get; set; }
+        [JsonProperty("cards")]
+        public List<BankCard> BankCards { get; set; }
+    }
+}
