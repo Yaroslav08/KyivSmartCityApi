7769ca88c0f54e04048cc85ca064cb0745f10876
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-10-29 21:31:27 +0200

    Added CommunalAddresses

---
+++ KyivSmartCityApi/KyivSmartCityApi/Models/CommunalAddresses.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace KyivSmartCityApi.Models
+{
+    public class CommunalAddresses
+    {
+        [JsonProperty("objects")]
+        public Object[] Objects { get; set; }
+    }
+
+    public class Object
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("address")]
+        public string Address { get; set; }
+        [JsonProperty("debt")]
+        public int Debt { get; set; }
+    }
+
+}
