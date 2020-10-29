9cb79e4100a614723f526a15649b275c004f523d
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-10-29 20:44:10 +0200

    Added ZonePrices entities

---
+++ KyivSmartCityApi/KyivSmartCityApi/Models/ZonePrices.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace KyivSmartCityApi.Models
+{
+    public class ZonePrices
+    {
+        [JsonProperty("prices")]
+        public Prices Prices { get; set; }
+    }
+    public class Prices
+    {
+        [JsonProperty("copy")]
+        public int Copy { get; set; }
+        [JsonProperty("zone_1")]
+        public int Zone1 { get; set; }
+        [JsonProperty("zone_2")]
+        public int Zone2 { get; set; }
+        [JsonProperty("zone_3")]
+        public int Zone3 { get; set; }
+    }
+
+}
