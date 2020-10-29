40cd32c8489dc53525c48ec6bf658c4404a9cfba
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-10-29 20:23:02 +0200

    Added some entities

---
+++ KyivSmartCityApi/KyivSmartCityApi/Models/Subscription.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KyivSmartCityApi.Models
+{
+
+    public class Subscription
+    {
+        [JsonProperty("notifications_enabled")]
+        public bool NotificationsEnabled { get; set; }
+        [JsonProperty("categories")]
+        public List<Category> Categories { get; set; }
+    }
+
+    public class Category
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("topics")]
+        public List<Topic> Topics { get; set; }
+    }
+
+    public class Topic
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("subscribed")]
+        public bool Subscribed { get; set; }
+    }
+}
