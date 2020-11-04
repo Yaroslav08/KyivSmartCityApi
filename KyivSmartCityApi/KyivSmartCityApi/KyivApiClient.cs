50c0ce32f066104e94acaa7574b6d56002abe375
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-11-04 15:18:45 +0200

    Changed name fromGetInfoFeed to GetInfoFeedById

--- KyivSmartCityApi/KyivSmartCityApi/KyivApiClient.cs
+++ KyivSmartCityApi/KyivSmartCityApi/KyivApiClient.cs
@@ -237,7 +237,7 @@ namespace KyivSmartCityApi
             return JsonConvert.DeserializeObject<HourlyParkingFreeTimes>(await httpClient.GetStringAsync($"api/hourly-parking/free-hours"));
         }
 
-        public async Task<FeedInfo> GetInfoFeed(string id)
+        public async Task<FeedInfo> GetInfoFeedById(string id)
         {
             return JsonConvert.DeserializeObject<FeedInfo>(await httpClient.GetStringAsync($"api/feed/{id}"));
         }
