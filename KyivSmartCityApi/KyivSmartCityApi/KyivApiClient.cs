fa35d844e41b5b96b88c4d0d2c3a8f6658c180be
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-11-16 12:12:47 +0200

    Remove unused namespaces

--- KyivSmartCityApi/KyivSmartCityApi/KyivApiClient.cs
+++ KyivSmartCityApi/KyivSmartCityApi/KyivApiClient.cs
@@ -7,7 +7,6 @@ using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace KyivSmartCityApi
