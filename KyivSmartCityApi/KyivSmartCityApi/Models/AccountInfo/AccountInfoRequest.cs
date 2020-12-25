6ab7007d0a5590f8f9b772a99da19c5cc58f9b11
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-12-25 20:20:02 +0200

    Added some entities

---
+++ KyivSmartCityApi/KyivSmartCityApi/Models/AccountInfo/AccountInfoRequest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+namespace KyivSmartCityApi.Models.AccountInfo
+{
+    public class AccountInfoRequest
+    {
+        [JsonPropertyName("fields")]
+        public List<FieldDetail> Fields { get; set; }
+
+        [JsonPropertyName("serviceKey")]
+        public string Key { get; set; } = "KYIVSMARTCARD";
+        [JsonPropertyName("amount")]
+        public string Amount { get; set; } = null;
+        [JsonPropertyName("multyCheckPaymentStepIndex")]
+        public int MultyCheckPaymentStepIndex { get; set; } = 0;
+
+        public AccountInfoRequest(string number)
+        {
+            Fields = new List<FieldDetail>
+            {
+                new FieldDetail
+                {
+                    Name = "Account",
+                    Value = number
+                },
+                new FieldDetail
+                {
+                    Name = "Agreement",
+                    Value = "50"
+                }
+            };
+        }
+    }
+}
---
+++ KyivSmartCityApi/KyivSmartCityApi/Models/AccountInfo/FieldDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+namespace KyivSmartCityApi.Models.AccountInfo
+{
+    public class FieldDetail
+    {
+        [JsonPropertyName("fieldName")]
+        public string Name { get; set; }
+        [JsonPropertyName("fieldValue")]
+        public string Value { get; set; }
+    }
+}
