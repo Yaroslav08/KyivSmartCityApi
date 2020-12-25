d943fd2cc0ccb0085a2ff78906356ed829953eb5
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-12-25 20:11:50 +0200

    Added AccountInfo entity

---
+++ KyivSmartCityApi/KyivSmartCityApi/Models/AccountInfo/AccountInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+namespace KyivSmartCityApi.Models.AccountInfo
+{
+    public class AccountInfo
+    {
+        [JsonPropertyName("Номер картки")]
+        public string CardNumber { get; set; }
+        [JsonPropertyName("Кількість поїздок")]
+        public string NumberOfTrips { get; set; }
+        [JsonPropertyName("Баланс на гаманці")]
+        public string BalanceOnTheWallet { get; set; }
+    }
+}
--- KyivSmartCityApi/KyivSmartCityApi/Models/AccountInfo/AccountInfoResponse.cs
+++ KyivSmartCityApi/KyivSmartCityApi/Models/AccountInfo/AccountInfoResponse.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 namespace KyivSmartCityApi.Models.AccountInfo
 {
     public class AccountInfoResponse
     {
-
+        [JsonPropertyName("accountInfo")]
+        public AccountInfo Info { get; set; }
     }
 }
